ffb6ba6538133f26f06b6445a16e113807afe190
Quin-Riley-A <quin.asselin1@gmail.com>
2022-12-11 07:29:54 -0500

    complete second constructor test for orders

--- BakeryOrders/Models/Order.cs
+++ BakeryOrders/Models/Order.cs
@@ -8,12 +8,16 @@ namespace Bakery.Models
     public string Description {get; set; }
     public int BreadCount {get; set; }
     public int PastryCount {get; set; }
-    public string date {get; set; }
-    private int Price {get; set; }
+    public string Date {get; set; }
+    public int Price {get; set; }
 
     public Order(string title, string description, string breadCount, string pastryCount, string date)
     {
-      
+      Title = title;
+      Description = description;
+      BreadCount = int.Parse(breadCount);
+      PastryCount = int.Parse(pastryCount);
+      Date = date;
     }
   }
 }
