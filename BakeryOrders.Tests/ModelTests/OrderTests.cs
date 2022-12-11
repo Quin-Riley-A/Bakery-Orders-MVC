2d203e56fe8f247caab91da2a23188fed6f39759
Quin-Riley-A <quin.asselin1@gmail.com>
2022-12-11 07:14:06 -0500

    create second constructor test for all user defined parameters

--- BakeryOrders.Tests/ModelTests/OrderTests.cs
+++ BakeryOrders.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,15 @@ namespace Bakery.Tests
       Order newOrder = new Order("order1Title", "order1Description", "3", "3", "September 3, 1821");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void ConstructorAssignsValues_ObjectValuesMatchArguments_Order()
+    {
+      Order newOrder = new Order("order1Title", "order1Description", "3", "3", "September 3, 1821");
+      Assert.AreEqual("order1Title", newOrder.Title);
+      Assert.AreEqual("order1Description", newOrder.Description);
+      Assert.AreEqual(3, newOrder.BreadCount);
+      Assert.AreEqual(3, newOrder.PastryCount);
+      Assert.AreEqual("September 3, 1821", newOrder.Date);
+    }
   }
 }
