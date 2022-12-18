7bb29f2df9e36ae6e17d63fa304c18911bc3fdee
Quin-Riley-A <quin.asselin1@gmail.com>
2022-12-18 06:08:09 -0500

    include collections for lists and build more RESTful pathing

--- BakeryOrders/Controllers/VendorController.cs
+++ BakeryOrders/Controllers/VendorController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace BakeryOrder.Controllers
@@ -6,12 +7,22 @@ namespace BakeryOrder.Controllers
   public class VendorController : Controller
   {
     [HttpGet("/vendors")]
-
+    public ActionResult Index()
+    {
+      List<Vendor> fullVendorList = Vendor.GetAll();
+      return View(fullVendorList);
+    }
 
     [HttpGet("/vendors/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newUserVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
     }
   }
 }
