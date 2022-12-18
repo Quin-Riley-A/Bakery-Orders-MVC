e2acf85b68c04e310412f0c9d76c9a2c2b979187
Quin-Riley-A <quin.asselin1@gmail.com>
2022-12-18 06:16:50 -0500

    confirm constructor assigns properties properly for vendor

--- BakeryOrders.Tests/ModelTests/VendorTests.cs
+++ BakeryOrders.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,11 @@ namespace Bakery.Tests
       Vendor newVendor = new Vendor("Bill's Bagels", "Bill sells bagels and various sundries");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    public void VendorClassConstructor_ObjectValuesMatchArguments_Vendor()
+    {
+      Vendor newVendor = new Vendor("Bill's Bagels", "Bill sells bagels and various sundries");
+      Assert.AreEqual("Bill's Bagels", newVendor.VendorName);
+      Assert.AreEqual("Bill sells bagels and various sundries", newVendor.VendorDescription);
+    }
   }
 }
